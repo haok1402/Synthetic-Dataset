2851dbbea824c0887b366248f574405a120046ba
haok1402 <haok1402@gmail.com>
2025-03-23 10:14:28 -0400

    each task shall grab the one gpu not always the first one and its global GPU IDs not always starts from 0

--- scripts/modules/textbook_step1.sh
+++ scripts/modules/textbook_step1.sh
@@ -7,6 +7,9 @@
 TASK_ID=$((SLURM_ARRAY_TASK_ID * SLURM_NPROCS + SLURM_PROCID))
 TASK_COUNT=$((SLURM_ARRAY_TASK_COUNT * SLURM_NPROCS))
 
+IFS=',' read -ra GPU_IDS <<< "$SLURM_JOB_GPUS"
+export CUDA_VISIBLE_DEVICES=${GPU_IDS[$SLURM_PROCID]}
+
 python3 -m sources.textbook \
     --task-id $TASK_ID --task-count $TASK_COUNT \
     --load-from "$DATASET_DIR/dclm-28B/*.jsonl" \
