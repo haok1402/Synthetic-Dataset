8875f87362a3db41a254a2c79536bd73455f6fc8
haok1402 <haok1402@gmail.com>
2025-03-29 03:53:46 -0400

    update the runtime environment once google cloud bucket is mounted

--- devconfig.sh
+++ devconfig.sh
@@ -1,6 +1,20 @@
-export WORKSPACE=/home/haok/MoE-Research
-export DATASET_DIR=$WORKSPACE/dataset
-mkdir -p $DATASET_DIR
+#!/bin/bash
+# Set up the runtime environment.
+
+export DATASET_PREFIX=MoE-Research/dataset
+export WEIGHTS_PREFIX=MoE-Research/weights
+
+export DATASET_DIR=/mnt/localssd/$DATASET_PREFIX
+export WEIGHTS_DIR=/mnt/localssd/$WEIGHTS_PREFIX
+mkdir -p $DATASET_DIR $WEIGHTS_DIR
+
+export BUCKET=gs://cmu-gpucloud-haok
+export GCP_DATASET_DIR=$BUCKET/$DATASET_PREFIX
+export GCP_WEIGHTS_DIR=$BUCKET/$WEIGHTS_PREFIX
+
+export NFS_MOUNT=$PWD/slurm-$SLURM_JOB_ID
+export SSD_MOUNT=/mnt/localssd/slurm-$SLURM_JOB_ID
+mkdir -p $NFS_MOUNT $SSD_MOUNT
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate synthetic
