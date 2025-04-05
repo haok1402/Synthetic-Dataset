0f194496aafaba39e54c063f567b41e187640b14
haok1402 <haok1402@gmail.com>
2025-04-05 10:37:48 -0400

    simplify the populate code

--- sources/textbook/populate.py
+++ sources/textbook/populate.py
@@ -1,21 +1,17 @@
 import argparse
 from pathlib import Path
-
-from google.cloud import storage
-from sources.queue import Queue
+from sources.textbook import store, queue
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bucket", type=str, required=True)
+    parser.add_argument("--model", type=str, required=True)
     parser.add_argument("--read-prefix", type=str, required=True)
     parser.add_argument("--save-prefix", type=str, required=True)
     parsed = parser.parse_args()
 
-    queue = Queue(topic="textbook")
-    store = storage.Client().bucket(parsed.bucket)
-
     for blob in store.list_blobs(prefix=parsed.read_prefix):
         queue.create({
+            "model": parsed.model,
             "read-from": blob.name,
             "save-into": Path(parsed.save_prefix, Path(blob.name).name).as_posix(),
         })
