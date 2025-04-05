0a674f08854091d4b711a0bca39de8f763da35ed
haok1402 <haok1402@gmail.com>
2025-04-05 00:44:42 -0400

    update the devconfig accordingly

--- devconfig.sh
+++ devconfig.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # Set up the runtime environment.
 
-export NFS_MOUNT=$PWD
-export SSD_MOUNT=/mnt/localssd
-export GCP_MOUNT=gs://cmu-gpucloud-haok
-
-export SSD_ENTRY=$SSD_MOUNT/synthetic-dataset
-export GCP_ENTRY=$GCP_MOUNT/MoE-Research/dataset
+export GCP_BUCKET=cmu-gpucloud-haok
+export GCP_PREFIX=MoE-Research/dataset
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate synthetic-dataset
